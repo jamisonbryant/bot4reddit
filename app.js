0dda8d51422112b546e81c10bf264f23cd088dd5
Jamison Bryant <robojamison@gmail.com>
2016-03-31 19:07:20 -0400

    Removed old, buggy, commented-out code

--- app.js
+++ app.js
@@ -40,34 +40,6 @@ redditBot.add('/profile',  [
     }
 ]);
 
-// // Add command dialog
-// redditBot.add('/', new builder.CommandDialog()
-//     .matches('^set name', builder.DialogAction.beginDialog('/profile'))
-//     .matches('^quit', builder.DialogAction.endDialog())
-//     .onDefault(function(session) {
-//         if (!session.userData.name) {
-//             session.beginDialog('/profile');
-//         } else {
-//             session.send('Hello %s!', session.userData.name);
-//         }
-//     })
-// );
-//
-// // Add profile dialog
-// redditBot.add('/profile', [
-//     function(session) {
-//         if (session.userData.name) {
-//             builder.Prompts.text(session, 'What would you like to change it to?');
-//         } else {
-//             builder.Prompts.text(session, 'Hi! What is your name?');
-//         }
-//     },
-//     function(session, results) {
-//         session.userData.name = results.response;
-//         session.endDialog();
-//     }
-// ]);
-
 // Set up and start Restify server
 var server = restify.createServer();
 server.post('/api/messages', redditBot.verifyBotFramework(), redditBot.listen());
