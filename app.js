316d7eec4c17c10fea0d1fa6c7d46bdd1c02fe30
Jamison Bryant <robojamison@gmail.com>
2016-03-31 17:57:12 -0400

    Added a route for saying hello to the user

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-var dotenv = require('dotenv').config();
+// Load dependencies
+var dotenv = require('dotenv');
 var restify = require('restify');
 var builder = require('botbuilder');
 
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: process.env.APP_ID, appSecret: process.env.APP_SECRET });
-bot.add('/', function(session) {
-    session.send('Hello World');
+// Configure environment
+dotenv.config();
+
+// Create bot
+var redditBot = new builder.BotConnectorBot({
+    appId: process.env.APP_ID,
+    appSecret: process.env.APP_SECRET
 });
 
-// Setup Restify Server
+// Add default dialog
+redditBot.add('/', function(session) {
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hello %s!', session.userData.name);
+    }
+});
+
+// Add profile dialog
+redditBot.add('/profile', [
+    function(session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function(session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+// Set up and start Restify server
 var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.post('/api/messages', redditBot.verifyBotFramework(), redditBot.listen());
+
 server.listen(process.env.port || 3978, function() {
     console.log('%s listening to %s', server.name, server.url);
 });
