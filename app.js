aa8104f45fdfd39e6829585337b4c373675d4109
Jamison Bryant <robojamison@gmail.com>
2016-03-31 17:53:30 -0400

    Added dotenv module for hiding sensitive data

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+var dotenv = require('dotenv').config();
 var restify = require('restify');
 var builder = require('botbuilder');
 
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
-bot.add('/', function (session) {
+var bot = new builder.BotConnectorBot({ appId: process.env.APP_ID, appSecret: process.env.APP_SECRET });
+bot.add('/', function(session) {
     session.send('Hello World');
 });
 
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.port || 3978, function() {
     console.log('%s listening to %s', server.name, server.url);
 });
